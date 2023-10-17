35fbcebef82e09f3930f6d8a6cfc3427a07f9d94
demidkinaa <demidkinaa@pgk.ru>
2023-10-17 21:59:49 +0300

    work with *.xlsx

--- handlers/vendor_codes.py
+++ handlers/vendor_codes.py
@@ -3,6 +3,7 @@ import os
 import tempfile
 import typing as t
 
+import openpyxl
 import requests
 from aiogram import F
 from aiogram import Router
@@ -12,6 +13,8 @@ from aiogram.types import (
     FSInputFile,
 )
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from openpyxl import Workbook
+from openpyxl.worksheet.worksheet import Worksheet
 
 from constants import PARAMS, EXAMPLE_VENDOR_CODES
 from markups import main_menu, results_by_parser_menu, list_vendor_codes_menu, main_menu_btn
@@ -22,7 +25,6 @@ from text_of_buttons import (
     GET_LIST_VENDOR_CODES_FILE, UPDATE_LIST_VENDOR_CODES,
 )
 from utils import get_text, add_history
-
 
 router: Router = Router()
 
@@ -168,7 +170,7 @@ async def get_vendor_codes(message: Message) -> list[str]:
     return vendor_codes
 
 
-@router.message(F.document)
+@router.message(F.document.file_name.endswith('.txt'))
 async def get_list_parsers_menu_(message: Message):
     document: Document = message.document
     file_bytes: io.BytesIO = await message.document.bot.download(file=document)
@@ -192,10 +194,108 @@ async def get_list_parsers_menu_(message: Message):
         pass
 
 
+@router.message(F.document.file_name.endswith('.xlsx'))
+async def get_list_parsers_menu_(message: Message):
+    document: Document = message.document
+    file_bytes: io.BytesIO = await message.document.bot.download(file=document)
+    wb: Workbook = openpyxl.load_workbook(filename=file_bytes)
+
+    # only first sheet
+    sheet: Worksheet = wb.worksheets[0]
+    # without header row
+    min_row: int = 2
+    result: dict[str, list[tuple[str, str]]] = {
+        'data': [row for row in sheet.iter_rows(values_only=True, min_row=min_row) if all(row)],
+    }
+
+    vendor_codes = await get_vendor_codes(message=message)
+
+    # fixme
+    good_vendor_codes: list[str] = []
+    good_makers: list[str] = []
+    bad_vendor_codes: list[str] = []
+
+    for maker, vendor_code in result['data']:
+        if str(vendor_code) in vendor_codes:
+            good_vendor_codes.append(str(vendor_code))
+            good_makers.append(maker.lower())
+        else:
+            bad_vendor_codes.append(vendor_code)
+
+    if bad_vendor_codes:
+        # fixme text
+        await message.answer(text=f'{bad_vendor_codes} not fount in global list')
+
+    # fixme url
+    response: requests.Response = requests.post(
+        url=f'{SERVER_ADDRESS}/run_all_pair_data',
+        json={
+            'vendor_codes': good_vendor_codes,
+            'makers': good_makers,
+            # fixme
+            'only_needful_vendor_code': False,
+        },
+    )
+
+    if response.ok:
+        response: list[str] = response.json()
+    else:
+        await message.answer(text='Файл не сформировался, наверное произошла ошибка, просьба написать в поддержку')
+    if response:
+        result_filepath: str = os.path.join(response[0])
+        document: FSInputFile = FSInputFile(path=result_filepath)
+
+        await message.answer_document(document=document, caption='Результат выполнения запроса')
+    else:
+        await message.answer(text='Файл не сформировался, наверное произошла ошибка, просьба написать в поддержку')
+
+
+@router.message(F.document)
+async def get_list_parsers_menu_(message: Message):
+    await message.answer(text='Данный тип файлов не поддерживается')
+
+
 @router.message(F.text == UPDATE_LIST_VENDOR_CODES)
 async def get_list_parsers_menu(message: Message):
     text: str = 'Для обновления списка артикулов необходимо загрузить файл следующего формата'
     await message.answer_document(document=EXAMPLE_VENDOR_CODES, caption=text)
+
+
+@router.message(F.text.split(',').len() == 2)
+async def get_weather_forecast(message: Message):
+    # fixme delete Params
+    add_history(user_id=message.from_user.id, data=message.text)
+
+    vendor_codes = await get_vendor_codes(message=message)
+
+    maker, vendor_code = message.text.split(',')
+
+    if vendor_code in vendor_codes:
+        data: dict[str, t.Any] = {
+            'vendor_code': vendor_code,
+            'maker': maker,
+            'only_needful_vendor_code': NEEDFUL_VENDOR_CODE in PARAMS[message.from_user.id],
+        }
+
+        # fixme url
+        response: requests.Response = requests.post(
+            url=f'{SERVER_ADDRESS}/run',
+            json=data,
+        )
+
+        if response.ok:
+            response: list[str] = response.json()
+        else:
+            await message.answer(text='Файл не сформировался, наверное произошла ошибка, просьба написать в поддержку')
+        if response:
+            result_filepath: str = os.path.join(response[0])
+            document: FSInputFile = FSInputFile(path=result_filepath)
+
+            await message.answer_document(document=document, caption='Результат выполнения запроса')
+        else:
+            await message.answer(text='Файл не сформировался, наверное произошла ошибка, просьба написать в поддержку')
+    else:
+        await message.answer(text='Этого артикула нет в списке артикулов')
 
 
 @router.message(F.text)
